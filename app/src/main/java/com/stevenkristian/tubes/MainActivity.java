27685b0d5fb68af1fdef87ffca8be5830797f19c
Steven <venvenkl12@gmail.com>
2020-10-05 19:19:13 +0700

    master file

--- app/src/main/java/com/stevenkristian/tubes/MainActivity.java
+++ app/src/main/java/com/stevenkristian/tubes/MainActivity.java
@@ -9,7 +9,6 @@ import android.os.Handler;
 import android.os.Looper;
 import android.util.Pair;
 import android.view.View;
-import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
