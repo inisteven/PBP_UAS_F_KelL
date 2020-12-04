ab19fa06d9c342fe1afe342367781b3587f02993
orznmi <ornaoza@gmail.com>
2020-12-04 15:47:24 +0700

    admin motor

---
+++ app/src/main/java/com/stevenkristian/tubes/admin/AdminHome.java
@@ -0,0 +1,117 @@
+package com.stevenkristian.tubes.admin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.google.android.material.navigation.NavigationView;
+import com.stevenkristian.tubes.R;
+
+public class AdminHome extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToogle;
+    private NavigationView nv;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_home);
+    }
+    private void init() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        mToogle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToogle);
+        mToogle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        nv = (NavigationView) findViewById(R.id.NavigatioView);
+        View view = nv.getHeaderView(0);
+    }
+
+    private void setAtribut() {
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                closeDrawer();
+                switch (id)
+                {
+                    case R.id.motor :
+                        loadFragment(new viewsMotorAdmin());
+                        break;
+                    case R.id.user :
+                        loadFragment(new viewsUserAdmin());
+                        break;
+                    case R.id.riwayatTransaksi :
+                        loadFragment(new viewHistoryAdmin());
+                        break;
+                    case R.id.keluar :
+                        AlertDialog.Builder builder = new AlertDialog.Builder(AdminHome.this);
+                        builder.setMessage("Anda yakin ingin keluar ?");
+                        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        });
+                        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                        AlertDialog alert = builder.create();
+                        alert.show();
+                        break;
+                    default :
+                        return true;
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(mToogle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void loadFragment(Fragment fragment) {
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction
+                .replace(R.id.mainFragment,fragment)
+                .commit();
+    }
+
+    private void closeDrawer() {
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+    }
+
+    @Override
+    public void onBackPressed() { }
+
+}
